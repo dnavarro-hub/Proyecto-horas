8161ec273c52cd0bfdd052890814df2b4bdbe364
dnavarro-hub <diego_navarro@loewe.com>
2026-08-10 09:01:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -112,8 +112,6 @@ class SubtareaDB(Base):
     activa          = Column(Boolean, default=True)
     created_at      = Column(DateTime, default=datetime.utcnow)
 
-# Borra todo y recrea limpio (solo usar una vez)
-Base.metadata.drop_all(bind=engine)
 Base.metadata.create_all(bind=engine)
 
 # ==================== INICIALIZACIÓN DE DATOS ====================
